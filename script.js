4103016f3ee84f068fa8b9e2e35819dac1fd134d
Vitor Lany <vitorlany99@gmail.com>
2022-06-16 17:04:52 -0300

    Filmes aleatorios

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 const api_key = '48117ef2f2ff8afded54743bb48a5315'
+destaqueCreate()
 
-filmesDestaques()
-function filmesDestaques() {
-    let url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=pt-BR`
-    let request = new XMLHttpRequest()
-    request.open("GET", url, true)
-    request.onload = () => {
-        construindoDestaques(request.response)
+function destaqueCreate() {
+    if (!localStorage.getItem("destaques")) {
+        var destaquesURL = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=pt-BR`
+        let request = new XMLHttpRequest()
+        request.open("GET", destaquesURL, true)
+        request.onload = () => {
+            localStorage.setItem("destaques", request.response)
+            data = JSON.parse(request.response)
+            construindoDestaques(data)
+        }
+        request.send()
+    } else {
+        let cache = localStorage.getItem("destaques")
+        data = JSON.parse(cache)
+        construindoDestaques(data)
     }
-    request.send()
 }
 
 function construindoDestaques(data) {
-    let listaFilmes = JSON.parse(data).results
+    let listaFilmes = data.results
     let destaquesID = document.getElementById("filmesDestaqueDIV")
-    for (let i = 0; i < 4; i++) {
+    let tamanho = 4
+    let tamanhoAleatorio = Math.floor(Math.random() * ((20) - tamanho + 1) + tamanho)
+    for (let i = (tamanhoAleatorio - 4); i < tamanhoAleatorio; i++) {
         let filme = listaFilmes[i]
         destaquesID.innerHTML += `
         <div class="col-12 col-md-6 card-destaque">
-              <a href="#">
-                <img src="imgs/filme-judas/principal.jpg" alt="">
-              </a>
-            </div>
+            <a href="#">
+                <img src="https://image.tmdb.org/t/p/original/${filme.poster_path}" alt="${filme.title}">
+            </a>
+        </div>
         `
     }
 }
